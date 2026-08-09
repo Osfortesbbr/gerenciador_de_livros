ffa58675bf86b0b5507b300da1a1f53a6981972e
Brayan <brayanbrandaor@gmail.com>
2026-08-09 16:57:01 -0300

    Adicionado a função de busca e início da função de ordenar

--- main.py
+++ main.py
@@ -23,13 +23,11 @@ def cadastrar_livro (titulo,autor,ano):
 
 def emprestimo_de_livro (codigo):
     linha = []
-    livro_encontrado = 0
 
     with open ('livros.csv','r',newline='') as arquivo:
         buscar_livro = csv.reader(arquivo)
         for itens in buscar_livro:
             if itens[3] == codigo:
-                livro_encontrado = 1
 
                 if itens[4] == "Disponivel":
                     itens[4] = "Indisponivel"
@@ -45,13 +43,11 @@ def emprestimo_de_livro (codigo):
 
 def devolver_livro (codigo):
     linha = []
-    livro_encontrado = 0
 
     with open ('livros.csv','r',newline='') as arquivo:
         buscar_livro = csv.reader(arquivo)
         for itens in buscar_livro:
             if itens[3] == codigo:
-                livro_encontrado = 1
                 if itens[4] == "Indisponivel":
                     itens[4] = "Disponivel"
                     print("O livro foi devolvido\n")
@@ -67,13 +63,33 @@ def devolver_livro (codigo):
 def listar_livros ():
     with open ('livros.csv','r') as arquivo:
         leitor = csv.DictReader(arquivo)
-        contador = 1
         for linha in leitor:
-                print(f"\n{contador}. Livro: {linha["titulo"]} ; Autor: {linha["autor"]} ; Ano: {linha["ano"]} ; Codigo: {linha["codigo"]} ; Status: {linha["status"]}\n")
-                contador += 1
+                print(f" Livro: {linha["titulo"]} ; Autor: {linha["autor"]} ; Ano: {linha["ano"]} ; Codigo: {linha["codigo"]} ; Status: {linha["status"]}\n")
+
+def busca_de_livro_autor(autor):
+    encontrou_autor = 0
+    with open ('livros.csv','r',newline='') as arquivo:
+        buscar_autor = csv.reader(arquivo)
+        for itens in buscar_autor:
+            if autor == itens[1]:
+                encontrou_autor = 1
+                print(f"Livro: {itens[0]} ; Autor: {itens[1]} ; Ano: {itens[2]} ; Codigo: {itens[3]} ; Status: {itens[4]}\n")
+        if encontrou_autor == 0:
+            print("Não foi encontrado nem um livro com esse autor\n")
+
+def busca_de_livro_titulo(titulo):
+    encontrou_livro = 0
+    with open ('livros.csv','r',newline='') as arquivo:
+        buscar_titulo = csv.reader(arquivo)
+        for itens in buscar_titulo:
+            if titulo == itens[0]:
+                encontrou_livro = 1
+                print(f"Livro: {itens[0]} ; Autor: {itens[1]} ; Ano: {itens[2]} ; Codigo: {itens[3]} ; Status: {itens[4]}\n")
+        if encontrou_livro == 0:
+            print("Não foi encontrado nem um livro com esse título\n")
 
 while True:
-    resposta_menu = input("Bem vindo(a) biblíoteca, o que gostaria de fazer?\n1 - [CADASTRAR]\n2 - [EMPRESTAR]\n3 -- [DEVOLVER]\n4 ---- [LISTAR]\n5 ---- [BUSCAR]\n6 --- [ORDENAR]\n7 ------ [SAIR]\n")
+    resposta_menu = input("Bem vindo(a) a biblíoteca, o que gostaria de fazer?\n1 - [CADASTRAR]\n2 - [EMPRESTAR]\n3 -- [DEVOLVER]\n4 ---- [LISTAR]\n5 ---- [BUSCAR]\n6 --- [ORDENAR]\n7 ------ [SAIR]\n")
     # Este é o menu inicial da biblíoteca, pergunta o que o usuario deseja fazer
 
     match resposta_menu:
@@ -100,12 +116,30 @@ while True:
             devolver_livro(codigo_livro)
 
         case "4":
-            print("Todos os livros estão a seguir:")
+            print("Todos os livros estão a seguir:\n")
             listar_livros()
 
         case "5":
-            pass
+            escolha_de_busca = int(input("Escolha um tipo de busca\n1 ---- [Por autor]\n2 -- [Por título]\n"))
+            if escolha_de_busca == 1:
+                nome_autor = input("Qual o nome do autor?\n")
+                busca_de_livro_autor(nome_autor)
+            elif escolha_de_busca == 2:
+                titulo_livro = input("Qual o titulo do livro?\n")
+                busca_de_livro_titulo(titulo_livro)
+            else:
+                print("Opção invalida\n")
         case "6":
-            pass
+            escolha_de_ordenar = int(input("Escolaha um tipo de ordenação\n1 -- [Por título]\n2 -- [Por autor]\n3 -- [Por ano]"))
+            if escolha_de_ordenar == 1:
+                pass
+            elif escolha_de_ordenar == 2:
+                pass
+            elif escolha_de_ordenar == 3:
+                pass
+            else:
+                print("Número invalida\n")
         case "7":
             break
+        case _:
+            print("Número inválido")
